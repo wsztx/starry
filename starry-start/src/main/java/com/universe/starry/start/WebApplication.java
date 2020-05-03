869faf02006b07ae5c00da5c93e942623b6d201f
zhoutianxiang <1390958241@qq.com>
2020-05-03 20:01:52 +0800

    tomcat部署配置

--- starry-start/src/main/java/com/universe/starry/start/WebApplication.java
+++ starry-start/src/main/java/com/universe/starry/start/WebApplication.java
@@ -3,15 +3,22 @@ package com.universe.starry.start;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy
 @MapperScan(value = {"com.universe.starry.*.mapper"})
 @SpringBootApplication(scanBasePackages = "com.universe.starry")
-public class WebApplication {
+public class WebApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebApplication.class);
+    }
 
     public static void main(String[] args) {
-        SpringApplication.run(com.universe.starry.start.WebApplication.class, args);
+        SpringApplication.run(WebApplication.class, args);
     }
 
 }
